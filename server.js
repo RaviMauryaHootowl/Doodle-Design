73a800ee1069ea8293d4aad263b107f40fcc1d3f
RaviMauryaHootowl <ravimaurya027@gmail.com>
2020-06-03 14:17:08 +0530

    Version 1.0.2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const {addUser, removeUser, getUser, getUsersInRoom ,getUserInRoomCount} = require('./users.js');
 
 
-if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV=== 'production'){
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
